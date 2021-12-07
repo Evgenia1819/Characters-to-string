050452299a443a14d6b307f81232ad3b2c3b79c7
Evgenia1819 <95176074+Evgenia1819@users.noreply.github.com>
2021-12-07 23:58:10 +0000

    Add files via upload

---
+++ Chars to String.js
@@ -0,0 +1,8 @@
+function charsToString(firstChar, secondChar, thirdChar) {
+   let result = firstChar + secondChar + thirdChar;
+   console.log(result);
+}
+
+charsToString('a', 'b', 'c');
+charsToString('%', '2','0');
+charsToString('1', '5','p');
